8ac26a6d88ba36b943c8fbd3b9013cf94591b02c
yurisp <yuripclive@live.ru>
2020-07-13 02:23:27 +0300

    добавил подсчет очков

--- TEST1/ContentView.swift
+++ TEST1/ContentView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     let rTarget = Double.random(in: 0..<1)
     let gTarget = Double.random(in: 0..<1)
     let bTarget = Double.random(in: 0..<1)
@@ -16,6 +17,17 @@ struct ContentView: View {
     @State var rGuess: Double
     @State var gGuess: Double
     @State var bGuess: Double
+    
+    @State var showAlert: Bool = false
+    
+    
+    func computeScore() -> Int {
+        let rDiff = rGuess - rTarget
+        let gDiff = gGuess - gTarget
+        let bDiff = bGuess - bTarget
+        let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+        return Int((1.0 - diff) * 100 + 0.5)
+    }
 
     var body: some View {
         VStack {
@@ -23,9 +35,14 @@ struct ContentView: View {
             Text("Угадайте данный цвет")
             Color(red: rGuess, green: gGuess, blue: bGuess)
             Text("R: G: B:")
-            Button(action: {}) {
+            Button(action: {
+                self.showAlert = true
+            }) {
                 Text("Угадать")
-            }
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Ваш счет:"),
+                      message: Text(String(computeScore())))
+            }.padding()
             ColorSlider(value: $rGuess, textColor: .red)
             ColorSlider(value: $gGuess, textColor: .green)
             ColorSlider(value: $bGuess, textColor: .blue)
