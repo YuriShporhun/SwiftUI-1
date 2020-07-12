ef0310375af46a06fbcd210bd67415277fed850b
yurisp <yuripclive@live.ru>
2020-07-12 23:00:52 +0300

    Добавил второй слайдер. Установил цвета для Color.

--- TEST1/ContentView.swift
+++ TEST1/ContentView.swift
@@ -9,22 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+
     var body: some View {
         VStack {
-            Color(red: 0.5, green: 0.5, blue: 0.5)
+            Color(red: rTarget, green: gTarget, blue: bTarget)
             Text("Угадайте данный цвет")
-            Color(red: 0.5, green: 0.5, blue: 0.5)
+            Color(red: rGuess, green: gGuess, blue: bGuess)
             Text("R: G: B:")
             Button(action: {}) {
                 Text("Угадать")
             }
             Slider(value: .constant(0.5))
-        } 
+            Slider(value: .constant(0.5))
+            Slider(value: .constant(0.5))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
     }
 }
